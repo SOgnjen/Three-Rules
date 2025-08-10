27d6c5b1f46050479cb57ef0992178872319af46
SOgnjen <srdicognjen@gmail.com>
2025-08-10 23:06:11 +0200

    Implemented basic Damage system

---
+++ Assets/Scripts/Level 1/Health/HealthController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    [SerializeField]
+    private float _currentHealth;
+    [SerializeField]
+    private float _maximumHealth;
+
+    public float RemainingHealth
+    {
+        get
+        {
+            return _currentHealth / _maximumHealth;
+        }
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        if (_currentHealth == 0)
+        {
+            return;
+        }
+
+        _currentHealth -= damageAmount;
+
+        if( _currentHealth < 0 )
+        {
+            _currentHealth = 0;
+        }
+    }
+
+    public void AddHealth(float amoutToAdd)
+    {
+        if(_currentHealth == _maximumHealth)
+        {
+            return;
+        }
+
+        _currentHealth += amoutToAdd;
+
+        if( _currentHealth > _maximumHealth )
+        {
+            _currentHealth = _maximumHealth;
+        }
+
+    }
+}
